700abb31b7fbbbb3a4793e75665be8b3c1010228
Matheus OS <matheus.sdoliveira@gmail.com>
2024-11-28 18:11:26 -0300

    alteração diretiva

--- src/app/directives/card-click.directive.ts
+++ src/app/directives/card-click.directive.ts
@@ -9,11 +9,9 @@ export class CardClickDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('click') onClick() {
-    this.renderer.setStyle(this.el.nativeElement, 'background-color', 'red');
     this.renderer.setStyle(this.el.nativeElement, 'transform', 'scale(1.05)');
     this.renderer.setStyle(this.el.nativeElement, 'background-color', '#f0f0f0');
     this.renderer.setStyle(this.el.nativeElement, 'transition', 'all 0.3s ease-in-out');
-
    
   }
 }
